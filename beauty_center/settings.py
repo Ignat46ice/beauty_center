47a5050b480bcb4dc2d553f49599b160cbc3e18d
Ignat46ice <ignat46ice@yahoo.com>
2024-05-22 19:15:55 +0300

    create project, models

--- beauty_center/settings.py
+++ beauty_center/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'reservation.apps.ReservationConfig',
+    "home",
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR), "static")
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = (os.path.join(BASE_DIR,"media"))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
